33e54bada9ca0700612d725f31dade1af0f6e374
Charvik <Charvik@LAPTOP-S0IS0QVR>
2022-07-26 10:23:23 +0530

    2nd day commits

--- src/PP/Second.java
+++ src/PP/Second.java
@@ -2,18 +2,33 @@ package PP;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 
 public class Second {
+	
+	public WebDriver driver;
   @Test
   public void f() {
+	  
+	  //1111111111111111111111111111
+	  //2222222222222222222222222222
+	  
+	  //33333333333333333333333333333333333
   }
   @BeforeTest
   public void beforeTest() {
+	  
+	  
+	  //5555555555555555555555555555555555
   }
 
   @AfterTest
   public void afterTest() {
+	  
+	  
+	  //yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+	  //9999999999999999999999999999999999999999999
   }
 
 }
